0e86eeec36a48db382274d5d2b70d9200b0df651
Mishary <58993063+8ZM@users.noreply.github.com>
2024-07-24 01:41:58 +0300

    Add files via upload

---
+++ script_ar.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['مصمم مواقع ويب', 'مبرمج بايثون', 'مصمم شبكات', 'مطور شغوف'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
